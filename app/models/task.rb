2bdbad6190693eb5645f2013b561fe748e46d1bd
jackclarke-parker <jack.clarke-parker@hnry.co.nz>
2025-05-03 22:49:19 +1200

    [FIX] Forgot to make Task inherit from ApplicationRecord

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,7 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/nrtest3 for more book information.
 #---
-class Task
+class Task < ApplicationRecord
   belongs_to :project
 
   def mark_completed(date = Time.current)
