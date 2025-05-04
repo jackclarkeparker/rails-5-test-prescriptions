09c8dea914f9e9bad5d80c5e22ef77ae259eeb8a
jackclarkeparker <jclarkeparker@gmail.com>
2025-05-05 00:40:59 +1200

    It handles a single string with malformed size

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -36,5 +36,12 @@ RSpec.describe CreatesProject do
       expect(tasks.size).to eq(1)
       expect(tasks.first).to have_attributes(title: "Start Things", size: 1)
     end
+
+    it "handles a single string with malformed size" do
+      creator = CreatesProject.new(name: "Project Runway", task_string: "Start Things:")
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: "Start Things", size: 1)
+    end
   end
 end
