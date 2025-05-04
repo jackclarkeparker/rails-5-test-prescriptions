09da85b6d6de097b683449c1f3618263249a0f8a
jackclarkeparker <jclarkeparker@gmail.com>
2025-05-05 00:55:49 +1200

    Refactor creates_project code to match Noel's code

--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -8,19 +8,24 @@ class CreatesProject
 
   def create
     build
-    project.tasks = convert_string_to_tasks
     project.save
   end
-
+  
   def build
     @project = Project.new(name: name)
+    project.tasks = convert_string_to_tasks
+    project
   end
 
   def convert_string_to_tasks
     task_string.split("\n").map do |task_substr|
-      title, size =  task_substr.split(":")
-      size = size && size.to_i > 0 ? size : 1
-      Task.new(title: title, size: size)
+      title, size_string =  task_substr.split(":")
+      Task.new(title: title, size: size_as_integer(size_string))
     end
+  end
+
+  def size_as_integer(size_string)
+    return 1 if size_string.blank?
+    [size_string.to_i, 1].max
   end
 end
