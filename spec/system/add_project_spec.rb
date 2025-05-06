3b7f18d62c79a5178180ca78403446387119f870
jackclarke-parker <jack.clarke-parker@hnry.co.nz>
2025-05-07 08:51:22 +1200

    Remove :pending on failure integration now that it passes

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "adding a project", type: :system do
     expect(page).to have_selector("#project_#{@project.id} .total-size", text: "8")
   end
 
-  it "does not allow a user to create a project without a name", :pending do
+  it "does not allow a user to create a project without a name" do
     visit new_project_path
     fill_in "Name", with: ""
     fill_in "Tasks", with: "Choose Fabric:3\nMark it Work:5"
